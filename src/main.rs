348b42f93e1f3419b13e583c73762f47b3f28e91
autopp <autopp.inc@gmail.com>
2024-04-02 00:01:49 +0900

    feat: add health endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-use axum::{
-    self,
-    routing::{on, MethodFilter},
-    Router,
-};
+use axum::{self, http::StatusCode, routing::get, Json, Router};
+
 use tokio::signal;
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
-    let app = Router::new().route("/", on(MethodFilter::GET, || async { "Hello, World!" }));
+    let app = Router::new().route("/health", get(health));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4000").await.unwrap();
     axum::serve(listener, app)
         .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
+}
+
+async fn health() -> (StatusCode, Json<serde_json::Value>) {
+    (StatusCode::OK, Json(serde_json::json!({ "status": "ok" })))
 }
 
 async fn shutdown_signal() {
