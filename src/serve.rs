3e8f996b33dbf86ed40a9b995348b24046294c10
autopp <autopp.inc@gmail.com>
2024-11-07 23:47:18 +0900

    feat: expose address

--- src/serve.rs
+++ src/serve.rs
@@ -1,3 +1,5 @@
+use std::net::SocketAddr;
+
 use axum::{http::StatusCode, routing::get, Json, Router};
 use tokio::{net::ToSocketAddrs, sync::oneshot::Sender, task::JoinHandle};
 
@@ -9,12 +11,17 @@ use crate::{
 pub struct ServerHandle {
     handle: JoinHandle<()>,
     close_tx: Sender<()>,
+    addr: SocketAddr,
 }
 
 impl ServerHandle {
     pub async fn shutdown(self) -> Result<(), String> {
         self.close_tx.send(()).map_err(|_| "".to_string())?;
         self.handle.await.map_err(|e| e.to_string())
+    }
+
+    pub fn addr(&self) -> SocketAddr {
+        self.addr
     }
 }
 
@@ -38,6 +45,7 @@ pub async fn serve<A: ToSocketAddrs>(
     let (close_tx, close_rx) = tokio::sync::oneshot::channel();
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    let addr = listener.local_addr().unwrap();
     let handle = tokio::spawn(async move {
         axum::serve(listener, app)
             .with_graceful_shutdown(async move {
@@ -49,9 +57,32 @@ pub async fn serve<A: ToSocketAddrs>(
             .unwrap();
     });
 
-    Ok(ServerHandle { handle, close_tx })
+    Ok(ServerHandle {
+        handle,
+        close_tx,
+        addr,
+    })
 }
 
 async fn health() -> (StatusCode, Json<serde_json::Value>) {
     (StatusCode::OK, Json(serde_json::json!({ "status": "ok" })))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[tokio::test]
+    async fn serve_and_shutdown() {
+        let server = serve(vec![], "0.0.0.0:0").await.unwrap();
+        let addr = server.addr();
+
+        let res = reqwest::get(format!("http://{}/health", addr))
+            .await
+            .unwrap();
+        server.shutdown().await.unwrap();
+
+        assert_eq!(200, res.status().as_u16());
+    }
 }
